532290d1f77de2d548abd1895407488da7190886
Mehmet <mehmetpekdemir06@hotmail.com>
2020-12-24 22:27:34 +0300

    One-to-One

---
+++ one-to-one/src/main/java/com/mehmetpekdemir/onetoone/dto/request/CreateSignUpRequest.java
@@ -0,0 +1,59 @@
+package com.mehmetpekdemir.onetoone.dto.request;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import com.mehmetpekdemir.onetoone.common.validator.FieldMatch;
+import com.mehmetpekdemir.onetoone.common.validator.UniqueUsername;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldMatch(first = "password", second = "confirmPassword", message = "{one-to-one.constraint.password.FieldMatch.message}")
+@ApiModel(value = "Create Sign-Up Request")
+public final class CreateSignUpRequest {
+
+	@NotNull
+	@UniqueUsername
+	@ApiModelProperty(value = "Username", required = true)
+	private String username;
+
+	@NotNull
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{one-to-one.constraint.confirmpassword.Pattern.message}")
+	@ApiModelProperty(value = "Password", required = true)
+	private String password;
+
+	@NotNull
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{one-to-one.constraint.confirmpassword.Pattern.message}")
+	@ApiModelProperty(value = "Confirm Password", required = true)
+	private String confirmPassword;
+
+	@NotNull
+	@ApiModelProperty(value = "First Name", required = true)
+	private String firstName;
+
+	@NotNull
+	@ApiModelProperty(value = "Last Name", required = true)
+	private String lastName;
+
+	@NotNull
+	@ApiModelProperty(value = "E-mail", required = true)
+	private String email;
+
+	@ApiModelProperty(value = "Birth Date")
+	private LocalDate birthDate;
+
+}
